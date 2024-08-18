f0a2c3b4f2dea22f3ada0738f3f59ff5f8f72d53
Keith Flynn <flynn.keith@gmail.com>
2024-08-18 17:26:57 -0400

    Add tests for Subtract, Multiply, Divide

--- Tests/KnowledgeCheck1_CalculatorTest/CalculatorTest.cs
+++ Tests/KnowledgeCheck1_CalculatorTest/CalculatorTest.cs
@@ -22,5 +22,53 @@ namespace KnowledgeCheck1_CalculatorTest
             //-- Assert
             Assert.AreEqual(expected, actual);
         }
+
+        [TestMethod]
+        public void SubtractionSubtracts()
+        {
+            //-- Arrange
+            Calculator calculator = new Calculator();
+            int subtractNumber1 = 1;
+            int subtractNumber2 = 2;
+            int expected = -1;
+
+            //-- Act
+            int actual = calculator.Subtract(subtractNumber1, subtractNumber2);
+
+            //-- Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void MultiplicationMultiplies()
+        {
+            //-- Arrange
+            Calculator calculator = new Calculator();
+            int multiplyNumber1 = 1;
+            int multiplyNumber2 = 2;
+            int expected = 2;
+
+            //-- Act
+            int actual = calculator.Multiply(multiplyNumber1, multiplyNumber2);
+
+            //-- Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void DivisionDivides()
+        {
+            //-- Arrange
+            Calculator calculator = new Calculator();
+            double divideNumber1 = 1;
+            double divideNumber2 = 2;
+            double expected = 0.5;
+
+            //-- Act
+            double actual = calculator.Divide(divideNumber1, divideNumber2);
+
+            //-- Assert
+            Assert.AreEqual(expected, actual);
+        }
     }
 }
